17cc90e249e34d25f72c2ff8a772719a02aa7742
github-action-benchmark <github@users.noreply.github.com>
2026-06-24 04:07:31 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 2785c4fdd9b4bb83274b3095948901a21abd1aeb

--- docs/benchmarks/binary-size/data.js
+++ docs/benchmarks/binary-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782187461812,
+  "lastUpdate": 1782274051297,
   "repoUrl": "https://github.com/thompson-tomo/otel-arrow",
   "entries": {
     "Benchmark": [
@@ -5383,6 +5383,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "linux-arm64-binary-size",
             "value": 99.6,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Swapnil Ashtekar",
+            "username": "swashtek",
+            "email": "46826200+swashtek@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "2785c4fdd9b4bb83274b3095948901a21abd1aeb",
+          "message": "etw_receiver: update attribute keys to use dot notation for consistency (#3346)\n\n# Change Summary\nupdate attribute keys to use dot notation for consistency\n\n## What issue does this PR close?\n* Closes #NNN\n\n## How are these changes tested?\n\n## Are there any user-facing changes?\nNo\n\n### Changelog\n\n<!--\nUser-facing changes need a .chloggen/*.yaml entry. Copy the\nTEMPLATE.yaml\nin go/.chloggen/ or rust/otap-dataflow/.chloggen/ and fill in the\nfields.\nIf not required, include `chore` in the PR title.\n-->\n\n* [x] Added a `.chloggen/*.yaml` entry, OR this PR is a `chore`\n(indicated in title).",
+          "timestamp": "2026-06-24T00:47:38Z",
+          "url": "https://github.com/thompson-tomo/otel-arrow/commit/2785c4fdd9b4bb83274b3095948901a21abd1aeb"
+        },
+        "date": 1782274048768,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "linux-amd64-binary-size",
+            "value": 111.51,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-binary-size",
+            "value": 98.91,
             "unit": "MB"
           }
         ]
